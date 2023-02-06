ee1eb44cc14be0bd2071ad085b4fac39cea86c09
root <root@localhost.local>
2023-02-06 13:41:52 +0300

    finished task_1

--- src/data/Commands.kt
+++ src/data/Commands.kt
@@ -5,8 +5,5 @@ class Commands {
     val startCommand = "/start"
     val helpCommand = "/help"
     val finishCommand = "/end"
-    val parkingCommand = "/park"
-    val returnCommand = "/return"
-    val parkInfoByCarCommand = "/park_info_by_car"
-    val parkInfoByPlaceCommand = "/park_info_by_place"
+
 }
